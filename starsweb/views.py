9075bc7db27cb6b9d16776c52e6d0bb25eed22d2
Jeff Bradberry <jeff.bradberry@gmail.com>
2011-09-24 00:24:23 -0400

    Made the default template_object_name 'game' and 'race' for the views.

--- starsweb/views.py
+++ starsweb/views.py
@@ -3,21 +3,24 @@ from django.db.models import Max
 import models
 
 
-def game_list(request, state=None, **kwargs):
+def game_list(request, state=None, template_object_name='game', **kwargs):
     qs = models.Game.objects.select_related('turn').annotate(
         generated=Max('turn__generated')).order_by('-generated')
     if state is None:
         state = request.GET.get('state', None)
     if state is not None:
         qs = qs.filter(state=state)
-    return object_list(request, qs, **kwargs)
+    return object_list(request, qs, template_object_name=template_object_name,
+                       **kwargs)
 
 
-def game_detail(request, **kwargs):
+def game_detail(request, template_object_name='game', **kwargs):
     qs = models.Game.objects.all()
-    return object_detail(request, qs, **kwargs)
+    return object_detail(request, qs,
+                         template_object_name=template_object_name, **kwargs)
 
 
-def race_detail(request, gameslug, **kwargs):
+def race_detail(request, gameslug, template_object_name='race', **kwargs):
     qs = models.Race.objects.filter(game__slug=gameslug)
-    return object_detail(request, qs, **kwargs)
+    return object_detail(request, qs,
+                         template_object_name=template_object_name, **kwargs)
