5f7f0cec24baf49181f2af334bdf3d2e69286f70
Jeff Bradberry <jeff.bradberry@gmail.com>
2011-09-24 00:02:25 -0400

    Created admin models for all currently defined models.

---
+++ starsweb/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from starsweb.models import Game, Race, Ambassador, Turn, Score
+
+
+class GameAdmin(admin.ModelAdmin):
+    fields = ('name', 'slug', 'description', 'markup_type', 'hosts',
+              'state', 'published')
+    list_display = ('name', 'created', 'state', 'published')
+    prepopulated_fields = {"slug": ("name",)}
+
+
+class RaceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'plural_name', 'game', 'player_number')
+    ordering = ('game',)
+    prepopulated_fields = {"slug": ("name",)}
+
+
+admin.site.register(Game, GameAdmin)
+admin.site.register(Race, RaceAdmin)
+admin.site.register(Ambassador)
+admin.site.register(Turn)
+admin.site.register(Score)
