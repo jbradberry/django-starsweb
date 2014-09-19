816aad82a2825637b411d64e40ba5b37b3e8152c
Jeff Bradberry <jeff.bradberry@gmail.com>
2014-09-18 20:42:53 -0400

    Cleaned up setup.py, to allow it to work with tox.

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 from distutils.core import setup
 import os
 
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+
 packages, data_files = [], []
 root_dir = os.path.dirname(__file__)
 if root_dir:
@@ -19,22 +30,23 @@ for dirpath, dirnames, filenames in os.walk('starsweb'):
         for f in filenames:
             data_files.append(os.path.join(prefix, f))
 
-setup(name='django-starsweb',
-      description='',
-      version="2.0.0dev",
-      author='Jeff Bradberry',
-      author_email='jeff.bradberry@gmail.com',
-      url='http://github.com/jbradberry/django-starsweb',
-      package_dir={'starsweb': 'starsweb'},
-      packages=packages,
-      package_data={'starsweb': data_files},
-      classifiers=['Development Status :: 2 - Pre-Alpha',
-                   'Environment :: Web Environment',
-                   'Framework :: Django',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: MIT License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python'
-                   'Topic :: Games/Entertainment :: Turn Based Strategy'],
-      long_description=open('README.rst').read(),
-      )
+setup(
+    name='django-starsweb',
+    description='',
+    version="2.0.0dev",
+    author='Jeff Bradberry',
+    author_email='jeff.bradberry@gmail.com',
+    url='http://github.com/jbradberry/django-starsweb',
+    package_dir={'starsweb': 'starsweb'},
+    packages=packages,
+    package_data={'starsweb': data_files},
+    classifiers=['Development Status :: 2 - Pre-Alpha',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python'
+                 'Topic :: Games/Entertainment :: Turn Based Strategy'],
+    long_description=read_file('README.rst'),
+)
