0b0b7c34ac6d3a38eb6acb7cf00d7b5a4a732d81
Jeff Bradberry <jeff.bradberry@gmail.com>
2025-12-22 10:45:00 -0500

    Incorporate the nh3 library to do html sanitization
    Looks like it is just drop-in, no real effort needed.

--- starsweb/markup.py
+++ starsweb/markup.py
@@ -1,5 +1,7 @@
-import markdown
 from django.conf import settings
+
+import markdown
+import nh3
 
 
 MARKUP_FILTER_OPTS = getattr(settings, 'MARKUP_FILTER_OPTS', {})
@@ -8,6 +10,5 @@ MARKUP_FILTER_OPTS = getattr(settings, 'MARKUP_FILTER_OPTS', {})
 def process(html):
     html = markdown.markdown(html)
     if html:
-        # FIXME: use a new html cleaner, probably nh3
-        return html
+        return nh3.clean(html, **MARKUP_FILTER_OPTS)
     return ''
